ae56eef14895e78f20886a13688096fa702c7b70
lunatic23333 <131398575+lunatic23333@users.noreply.github.com>
2026-01-14 16:34:30 +0800

    Add files via upload

---
+++ project/PythonBase/py_print.py
@@ -0,0 +1,18 @@
+# # *args 想输出多少，就输出多少
+# print()
+# print(123)
+# print(12, 243, 43, 24)
+# print("hello world", "python 2026", "中文也行")
+
+
+# # sep 负责控制多个参数之间的分隔符
+# print(123, 243, 43, 24, sep=",")
+# print("hello world", "python 2026", "中文也行", sep="*")
+# print("hello world", "python 2026", "中文也行", sep="\n")
+# print(123, 243, 43, 24, sep="\t")
+# print(123, 234, 43124, sep="***")
+
+# end 负责控制输出的换行符
+# 默认换行符是\n
+print(23132, end="@@@")
+print(5432)
---
+++ project/PythonBase/py_variable.py
@@ -0,0 +1,46 @@
+# 定义变量
+# 变量名 = 值
+
+# # 直接赋值
+# age = 22
+# phone = "12345678909"
+# name = "浪浪"
+# print(name, age, phone, sep="\n")
+
+# # 将一个变量赋给另一个变量
+# age1 = age + 1
+# print(age1)
+#
+# # 表达式赋值
+# age2 = 2 * 12 + 1
+# print(age2)      # 作为变量输出，先赋值再使用
+# print("age2")    # 作为变量名，输出变量名
+
+# # 动态赋值
+# variable = 123
+# print(variable)
+# variable = 123.34
+# print(variable)
+# variable = "elbairav"
+# print(variable)
+# variable = {"name": name, "age": age}
+# print(variable)
+
+# # 序列赋值，必须一一对应
+# Chiense_score, English_score, Math_score = 98, 100, 100
+# print(Chiense_score, English_score, Math_score, sep="\n")
+
+# 变量命名
+# dfas = 23412
+# _dsaf = 3412
+# fsa_fsdfs = 1234
+# a123_ = _dsaf
+# print(dfas, _dsaf, fsa_fsdfs, a123_)
+# username = "elbairav"
+# print(username)
+# user_name = "elbairav"
+# print(user_name)
+# UserName = "elbairav"
+# print(UserName)
+# userName = "elbairav"
+# print(userName)
